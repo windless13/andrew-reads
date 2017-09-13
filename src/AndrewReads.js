8754268221f60442d3788d3531c4bb102fe56025
Andrew  Lee <andrewlee@andrews-mbp.gateway.sonic.net>
2017-09-13 00:22:21 -0700

    Add scss preprocessor

---
+++ src/AndrewReads.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './AndrewReads.css';
+
+class AndrewReads extends Component {
+    render() {
+        return (
+            <div className="AndrewReads">
+                <div className="test">
+                    Hello, world
+
+                    <span className="test__span"> My Test </span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AndrewReads;
